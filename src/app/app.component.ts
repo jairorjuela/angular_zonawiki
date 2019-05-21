94b07314f45d31e8edfafd1b19569ac42dfc39cb
jairorjuela <genius85862@gmail.com>
2019-05-20 23:04:00 -0500

    Task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   welcome : string;
-  title = 'TallerBase2';
-  frameworks : {
-      framework: string,
-      owner : string,
-      release : string
+  title = 'Tarea taller 2';
+  tasks : {
+      task: string,
+      responsible : string
   }[];
   public show:boolean = false
 
 
-  toggle(framework) {
-    framework.show = !framework.show;
+  toggle(task) {
+    task.show = !task.show;
   }
 
   constructor(){
-      this.welcome = "Puedes ocultar/mostrar cada fila!!!!"
+      this.welcome = "Tarea del taller número 2"
 
-      this.frameworks = [{
-          framework : "ANGULAR",
-          owner: " GOOGLE",
-          release : "14 Septiembre 2016"
+      this.tasks = [{
+          task : "Revisar agenda",
+          responsible: "Pepito"
       },
       {
-          framework : "REACT",
-          owner: " FACEBOOK",
-          release : "Marzo 2013"
+          task : "Trotar",
+          responsible: "Fulando"
       },
       {
-          framework : "VUE",
-          owner: "COMMUNITY",
-          release : "Febrero 2014"
+          task : "Comer",
+          responsible: "Perensejo"
       }]
   };
 }
