04e8685de2b38eafac147f7da9b9ae17838b23f7
jairorjuela <genius85862@gmail.com>
2019-05-20 21:37:34 -0500

    Task 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Tas1Component } from './components/tas1/tas1.component';
-import { Task1Component } from './components/task1/task1.component';
-import { Task2Component } from './components/task2/task2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Tas1Component,
-    Task1Component,
-    Task2Component
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
